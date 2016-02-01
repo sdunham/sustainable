5ade29c19d1aa7279c04fff1dcf21335d11f1ca0
sdunham <dunham.scott@gmail.com>
2016-01-31 18:01:45 -0800

    Team page template and fields

--- wp-content/themes/sustainable/team.php
+++ wp-content/themes/sustainable/team.php
@@ -8,4 +8,10 @@ $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
 
+$context['posts'] = Timber::get_posts('post_type=team-cpt&posts_per_page=-1');
+
+$context['content_column_1'] = $post->content(1);
+$context['content_column_2'] = $post->content(2);
+$context['content_has_columns'] = strpos($post->post_content  , '<!--nextpage-->' ) !== false;
+
 Timber::render( 'team.twig', $context );
